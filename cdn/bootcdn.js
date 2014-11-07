110c20156a8108429ddb9b8ad25304b595c66807
hhlin <smartken0824@gmail.com>
2014-11-07 18:59:27 +0800

    Update bootcdn.js

--- cdn/bootcdn.js
+++ cdn/bootcdn.js
@@ -12,60 +12,72 @@
     
     var writeScript=function(src){
          doc.write("<script type='text/javascript' src=\""+src+"\" ><"+"/script>");
-    }
+    };
     
     var writeStyle=function(href){
          doc.write("<link rel=\"stylesheet\" type=\"text/css\"  href=\""+href+"\" />");
-    }
+    };
+    
+    bootcdn["importScript"]=function(src){
+         writeScript(base+src);
+    };
+    
+    bootcdn["importStyle"]=function(href){
+         writeStyle(base+href);
+    };
     
     bootcdn["importJquery"]=function(version){
          version=version||"2.1.1";
-         writeScript(base+"/jquery/"+version+"/jquery.min.js");
-    }
+         importScript("/jquery/"+version+"/jquery.min.js");
+    };
     
     bootcdn["importJqueryui"]=function(version){
          version=version||"1.11.2";
-         writeScript(base+"/jqueryui/"+version+"/jquery-ui.min.js");
-         writeStyle(base+"/jqueryui/"+version+"/jquery-ui.min.css")
-    }
+         importScript("/jqueryui/"+version+"/jquery-ui.min.js");
+         importStyle("/jqueryui/"+version+"/jquery-ui.min.css")
+    };
     //yui/yui/yui-min.js
     bootcdn["importYui"]=function(version){
          version=version||"3.18.0/";
-         writeScript(base+"/yui/"+version+"/yui/yui-min.js");
-    }
+         importScript("/yui/"+version+"/yui/yui-min.js");
+    };
     
     bootcdn["importBootstrap"]=function(version){
          version=version||"3.3.0";
-         writeScript(base+"/bootstrap/"+version+"/js/bootstrap.min.js");
-         writeStyle(base+"/bootstrap/"+version+"/css/bootstrap.min.css")
-    }
+         importScript("/bootstrap/"+version+"/js/bootstrap.min.js");
+         importStyle("/bootstrap/"+version+"/css/bootstrap.min.css")
+    };
     
      bootcdn["importKnockout"]=function(version){  
          version=version||"3.2.0";
-         writeScript(base+"/knockout/"+version+"/knockout-min.js");
-    }
+         importScript("/knockout/"+version+"/knockout-min.js");
+    };
     
     bootcdn["importD3"]=function(version){  
          version=version||"3.4.13";
-         writeScript(base+"/d3/"+version+"/d3.min.js");
-    }
+         importScript("/d3/"+version+"/d3.min.js");
+    };
     
     bootcdn["importThreejs"]=function(version){  
          version=version||"r69";
-         writeScript(base+"/three.js/"+version+"/three.min.js");
-    }
+         importScript("/three.js/"+version+"/three.min.js");
+    };
     
     bootcdn["importDatejs"]=function(version){  
          version=version||"1.0";
-         writeScript(base+"/datejs/"+version+"/date.min.js");
-    }
+         importScript("/datejs/"+version+"/date.min.js");
+    };
     
     bootcdn["importJsurl"]=function(version){  
          version=version||"1.8.4";
-         writeScript(base+"/js-url/"+version+"/url.min.js");
-    }
+         importScript("/js-url/"+version+"/url.min.js");
+    };
     
     
+    bootcdn["importLinq"]=function(version){  
+         version=version||"2.2.0.2";
+         importScript("/linq.js/"+version+"/jquery.linq.min.js");
+    };
     
     win['Bootcdn']=bootcdn;
   
