16c9c97bf5d194d6706ea6fb7f95205262fcdce7
hhlin <smartken0824@gmail.com>
2014-11-16 14:06:04 +0800

    Update rawgithub.js

--- cdn/rawgithub.js
+++ cdn/rawgithub.js
@@ -29,7 +29,7 @@
      cdn["importScript"]=importScript;
     cdn["importStyle"]=importStyle;
 
-    cdn["importMetro"]=function(version){
+    cdn["importMetroUI"]=function(version){
          writeStyle("/olton/Metro-UI-CSS/master/min/metro-bootstrap.min.css");
           writeStyle("/olton/Metro-UI-CSS/master/min/iconFont.min.css");
     };
