499c020318646096865e31f438cc4462c740c8b7
hhlin <smartken0824@gmail.com>
2014-11-21 09:58:58 +0800

    Update rawgithub.js
    add persistence

--- cdn/rawgithub.js
+++ cdn/rawgithub.js
@@ -37,9 +37,18 @@
    
     cdn["importJqueryui_bootstrap"]=function(version){
        version=version||"1.10.3.theme";
-       importStyle("jquery-ui-bootstrap/jquery-ui-bootstrap/masterbs3/css/custom-theme/jquery-ui-"+version+".css");
+       importStyle("/jquery-ui-bootstrap/jquery-ui-bootstrap/masterbs3/css/custom-theme/jquery-ui-"+version+".css");
     }
    
+    //persistencejs
+    cdn["importPersistencejs"]=function(plugins){
+       //version=version||"1.10.3.theme";
+       var prefix="persistence";
+       importScript("/coresmart/persistencejs/master/lib/"+prefix+".js");
+       for(var plugin in plugins){
+        importScript("/coresmart/persistencejs/master/lib/"+prefix+"."+plugin+".js");
+       }
+    }
     
    
     win["Rawgithub"]=cdn;
